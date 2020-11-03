fc8847fa0ba4175c30e2a5f4523d0b2fc829c01b
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2020-11-03 10:07:30 +0700

    Add login

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+    }
+}
