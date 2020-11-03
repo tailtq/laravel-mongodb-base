1e1c858b62f58b5c6f616372bfdb7892de93c794
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-11-03 17:19:35 +0700

    Update html sidebar

--- app/Traits/RequestAPI.php
+++ app/Traits/RequestAPI.php
@@ -11,16 +11,17 @@ trait RequestAPI
     public function getResponseAPI() //Retrieve
     {
         $client = new Client();
-        $response = $client->get('http://api.example.com/v1/retrieve');
-        $body = json_decode($response->getBody(), TRUE);
+        $response = $client->get('https://5fa119bf2541640016b6a6d8.mockapi.io/Tet');
+        $body = json_decode($response->getBody(), true);
+        dd($body);
         return $body;
     }
 
-    public function postResponseAPI() //Retrieve
+    public function postResponseAPI()
     {
         $client = new Client();
         $response = $client->post('http://api.example.com/v1/retrieve');
-        $body = json_decode($response->getBody(), TRUE);
+        $body = json_decode($response->getBody(), true);
         return $body;
     }
 }
