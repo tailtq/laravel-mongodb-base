1ec4f8102d9738820c9bbab195cfd108c845342b
Tai.Ltq <tai.ltq@terra-drone.co.jp>
2020-11-30 17:12:34 +0700

    Fix bug frame_to

--- src/resources/views/layout/footer.blade.php
+++ src/resources/views/layout/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-  <p class="text-muted text-center text-md-left">Copyright © 2020 <a href="https://www.nobleui.com" target="_blank">Greenglobal</a>. All rights reserved</p>
+  <p class="text-muted text-center text-md-left">Copyright © 2020 <a href="https://greenglobal.vn" target="_blank">Green Global</a>. All rights reserved</p>
 </footer>
