6e556ea2513e7d7c0983e4df48c9da83818502c6
Tai Le <ltquoctaidn98@gmail.com>
2021-01-26 09:24:07 +0700

    Update query

--- src/app/Traits/AnalysisTrait.php
+++ src/app/Traits/AnalysisTrait.php
@@ -13,13 +13,14 @@ trait AnalysisTrait
         return [
             DB::raw("(SELECT COUNT(*) FROM objects WHERE objects.process_id = $processId) as total_appearances"),
             DB::raw("
-                (SELECT COUNT(*) FROM objects as OO WHERE OO.id in (
-                    SELECT min(objects.id) as unq_identity_id
+                (SELECT (SELECT COUNT(*) FROM objects as OO WHERE OO.id in (
+                    SELECT min(objects.id)
                         FROM objects
                         WHERE objects.process_id = $processId
-                        GROUP BY IFNULL(objects.cluster_id, UUID())
-                )) as total_objects
-            "),
+                        AND objects.cluster_id IS NOT NULL
+                        GROUP BY objects.cluster_id
+                )) + (SELECT COUNT(*) FROM objects WHERE process_id = $processId AND cluster_id IS NULL)) AS total_objects
+            "),  # group by uuid is computationally expensive than combination of 2 select count
             DB::raw("
                 (SELECT COUNT(*) FROM objects as OO WHERE id in (
                     SELECT min(objects.id) FROM objects INNER JOIN clusters ON clusters.id = objects.cluster_id
