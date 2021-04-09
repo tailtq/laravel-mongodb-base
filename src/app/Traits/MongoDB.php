ba850bc02e0d0c1d6064ad452eb243dffb37d09f
tailtq <ltquoctaidn98@gmail.com>
2021-04-09 09:57:18 +0700

    Update mongodb trait

--- src/app/Traits/MongoDB.php
+++ src/app/Traits/MongoDB.php
@@ -2,20 +2,48 @@
 
 namespace App\Traits;
 
+use MongoDB\Driver\Manager;
+use MongoDB\Collection;
+
 trait MongoDB
 {
-    public static function queryMongo($collection)
+    protected $collectionName;
+
+    /**
+     * @param $collectionName
+     * @return Collection|Manager
+     */
+    public function getCollection($collectionName)
     {
         $databaseName = env('DB_DATABASE');
         $username = env('DB_USERNAME');
         $password =  env('DB_PASSWORD');
-        $urlServer = env('DB_HOST') . env('DB_PORT');
+        $urlServer = env('DB_HOST') . ':' . env('DB_PORT');
+        $manager = new Manager("mongodb://$username:$password@$urlServer/$databaseName?authSource=admin");
 
-        $manager = new \MongoDB\Driver\Manager("mongodb://$username:$password@$urlServer/$databaseName?authSource=admin");
+        return new Collection($manager, $databaseName, $collectionName);
+    }
 
-        $collection = new \MongoDB\Collection($manager, $databaseName, $collection);
+    /**
+     * @param $condition
+     * @return array
+     */
+    public function find($condition): array
+    {
+        $collection = $this->getCollection($this->collectionName);
 
-        $results = $collection->find();
-        return iterator_to_array($results);
+        return iterator_to_array($collection->find($condition));
+    }
+
+    /**
+     * @param $conditionArr
+     * @param array $options
+     * @return array
+     */
+    public function aggregate($conditionArr, $options = []): array
+    {
+        $collection = $this->getCollection($this->collectionName);
+
+        return iterator_to_array($collection->aggregate($conditionArr, $options));
     }
 }
